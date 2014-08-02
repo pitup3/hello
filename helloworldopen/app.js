30050cd38f3e20c9c1f633f253bc004231103631
pitup3 <pitambarp3@yahoo.com>
2014-08-02 17:38:42 +0530

    Update app.js

--- helloworldopen/app.js
+++ helloworldopen/app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-var port =  process.env.OPENSHIFT_NODEJS_PORT || 80;   // Port 8080 if you run locally
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
